db548fac981dd23b507c3bb99d0f307b11b02072
zjh <1512090646@qq.com>
2026-05-22 23:11:06 +0800

    fix: 注释启动调用 AI 大模型

--- src/main/java/org/zjh/aiagent/demo/invoke/SpringAiAiInvoke.java
+++ src/main/java/org/zjh/aiagent/demo/invoke/SpringAiAiInvoke.java
@@ -1,9 +1,7 @@
 package org.zjh.aiagent.demo.invoke;
 
 import jakarta.annotation.Resource;
-import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,9 +16,9 @@ public class SpringAiAiInvoke implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        AssistantMessage message = dashScopeChatModel.call(new Prompt("你是谁？"))
-                .getResult()
-                .getOutput();
-        System.out.println(message.getText());
+//        AssistantMessage message = dashScopeChatModel.call(new Prompt("你是谁？"))
+//                .getResult()
+//                .getOutput();
+//        System.out.println(message.getText());
     }
 }
